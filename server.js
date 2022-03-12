08531542c397f83ed120c604382b4d0e76f91dbc
Will Yazdani <yazd0006@gmail.com>
2022-03-12 16:19:57 -0600

    added routes to server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
@@ -6,9 +7,25 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+//index page
+app.get('/', (req, res) => {
+    res.sendFile(__dirname, '/index.html');
+});
+
+//notes page
 app.get('/notes', (req, res) => {
-    console.log('Here');
     res.sendFile(path.join(__dirname, '/public/notes.json'));
-})
+});
+
+//API
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, NoteData) => {
+        if (err) {
+            console.log(err);
+        }
+        const notes = JSON.parse(NoteData);
+        res.json(notes)
+    })
+});
 
 app.listen(PORT)
