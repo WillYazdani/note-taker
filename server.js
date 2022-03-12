59dc2cf7e4f98a55c4b59cd95cfe2e74fb3fc642
Will Yazdani <yazd0006@gmail.com>
2022-03-12 13:02:15 -0600

    inital note route added

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log('here');
-    res.send('hello')
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+    console.log('Here');
+    res.sendFile(path.join(__dirname, '/public/notes.json'));
 })
 
-app.listen(3000)
+app.listen(PORT)
