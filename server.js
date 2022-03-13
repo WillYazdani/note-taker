87c5d0fbbae3c77b8f4efb052e60a4ba84cfc2c5
Will Yazdani <yazd0006@gmail.com>
2022-03-12 19:39:36 -0600

    added heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: true}));
@@ -14,12 +14,7 @@ app.use(express.json());
 let notes = fs.readFileSync('./db/db.json');
 notes = JSON.parse(notes);
 
-//home page
-app.get('/', (req, res) => {
-    res.sendFile(__dirname, '/index.html');
-});
-
-//notes page
+//notes
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
@@ -32,8 +27,10 @@ app.get('/api/notes', (req, res) => {
 //add notes
 app.post('api/notes', (req, res) => {
     const addNote = req.body;
-
+    notes.push(addNote);
     res.end();
 });
 
-app.listen(PORT)
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
